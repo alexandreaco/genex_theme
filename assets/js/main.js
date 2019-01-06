9861d6f8172dce07c03ae2ee33257435298d6fab
Alex Defreitas <alex@motel.is>
2019-01-06 12:30:19 -0500

    Cleans up project slideshow

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,26 +13,17 @@ $(document).ready(function(){
   });
 
   var slideshow = $('.project-slideshow').slick({
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true,
-    appendArrows: false,
     autoplay: true,
-    pauseOnHover: false
+    pauseOnHover: false,
+    draggable: true,
+    arrows: false,
+    dots: true,
+    fade: true,
+    speed: 900,
+    infinite: true,
+    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+    touchThreshold: 100
   });
-
-  $('.project-slideshow').on('afterChange', function(e, slick, currentSlide) {
-    var currentSlideIndex = currentSlide + 1
-    $('[data-go-to-slide=' + currentSlideIndex + ']').addClass('active').siblings().removeClass('active')
-  });
-
-  $("[data-go-to-slide]").click(function(el) {
-    var slideIndex = $(el.target).data("go-to-slide")
-    $('.project-slideshow').slick('slickGoTo', slideIndex, true);
-  });
-
 
   /* Nav Dropdown */
   $('[data-nav-button]').click(function(e) {
